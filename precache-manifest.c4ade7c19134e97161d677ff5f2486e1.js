bc09feebbe7a35335c11f13c2a331ad8fa7dfa06
sttwarrior <ryan.yenchen.liu@berkeley.edu>
2020-02-24 00:03:18 -0800

    Updates

---
+++ precache-manifest.c4ade7c19134e97161d677ff5f2486e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe1cf43714e3ff3ebd8830fe102d5cc",
+    "url": "/SoSHero/index.html"
+  },
+  {
+    "revision": "644e6e4180880a387d48",
+    "url": "/SoSHero/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "644e6e4180880a387d48",
+    "url": "/SoSHero/static/js/2.d720eb2f.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/SoSHero/static/js/2.d720eb2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6f358256adf5aecef0",
+    "url": "/SoSHero/static/js/main.3032d38d.chunk.js"
+  },
+  {
+    "revision": "b5562752fc6b6a4d76c3",
+    "url": "/SoSHero/static/js/runtime-main.c9c54a0c.js"
+  },
+  {
+    "revision": "ee7bd910fe90d98c8bd186f403839381",
+    "url": "/SoSHero/static/media/unknown.ee7bd910.jpg"
+  }
+]);
